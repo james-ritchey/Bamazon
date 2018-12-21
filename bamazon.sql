17ff7e3b807a7663e04666188c425b4a49997b8a
Jim Ritchey <jim0ritchey@gmail.com>
2018-12-20 21:41:51 -0500

    Pushing initial files

---
+++ bamazon.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM products
+LIMIT 0, 1000
+
+-- Date: 2018-12-20 21:21
+*/
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (1,'Becho','Electronics',24.99,100);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (2,'Blank Book','Books',12.99,250);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (3,'Left-Handed Ladle','Cooking',14.99,175);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (4,'Blinker Fluid','Automotive',12.99,250);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (5,'Smoke Stirrer','Camping',9.99,125);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (6,'Wireless Garden Hose','Lawn and Garden',249.99,200);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (7,'Solar Powered Reading Light','Books',13.99,300);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (8,'Waterproof Soap','Bathroom',16.99,145);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (9,'Brushless Toothbrush','Bathroom',7.99,250);
+INSERT INTO `products` (`item_id`,`product_name`,`department_name`,`price`,`stock_quantity`) VALUES (10,'Organic Vaseline','Bathroom',27.99,135);
---
+++ bamazonCustomer.js
@@ -0,0 +1,69 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bamazon"
+  });
+  
+
+connection.connect(function(err) {
+    if (err) throw err;
+    afterConnection();
+});
+
+function afterConnection() {
+    var items = {};
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        res.forEach(item => {
+            console.log("ID: " + item.item_id + "\nName: " + item.product_name + "\nPrice: " + item.price + "\n");
+            items[item.item_id] = {
+                product_name: item.product_name,
+                department: item.department_name,
+                price: item.price,
+                stock: item.stock_quantity
+            }
+        })
+        //console.log(items);
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the ID of the product you wish to buy?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "How many do you wish to buy?",
+                name: "amount"
+            }
+        ]).then(response => {
+            var itemID = response.id;
+            var amount = response.amount;
+            if(items[itemID].stock >= amount) {
+                var sql = "UPDATE products SET stock_quantity = stock_quantity - " + amount + " WHERE item_id = " + itemID;
+                connection.query(sql, function(err, res) {
+                    if (err) {
+                        return console.log(err);
+                    };
+                    console.log("Purchase has been made!");
+                    afterConnection();
+                });
+            }
+            else {
+                console.log("Insufficient quantity to make your purchase!");
+            }
+        })
+    });
+    
+}
+  
